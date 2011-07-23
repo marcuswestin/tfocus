4b91370a0c633ec88ed3cd28528cb8ab811aaa43
Marcus Westin <marcus.westin@gmail.com>
2011-07-23 05:39:51 -0400

    Show an input field when you click on a title, and allow for modifying titles

--- src/client/ios/WorkScreen.js
+++ src/client/ios/WorkScreen.js
@@ -27,9 +27,31 @@ module.exports = Class(WorkScreen, function() {
 	}
 	
 	this._selectTask = function(task, node) {
-		var offsetTop = node.getElement().offsetTop,
-			height = getDocumentHeight(this._doc) - this._header.getOffset().height - this._footer.getOffset().height
-		node.style({ height:height })
-		scrollability.scrollTo(this._scrollDiv.getElement(), 0, -offsetTop, 200)
+		if (!this._taskFocus) {
+			this._taskFocus = DIV('taskFocus',
+				this._taskTitleInput = INPUT('title', { 
+					keypress:bind(this, this._onFocusedTaskTitleKeyPress),
+					blur:bind(this, this._onFocusedTaskTitleBlur)
+				})
+			)
+		}
+		this._focusedTask = task
+		this._taskFocus.appendTo(this._body)
+		this._taskFocus.style({ position:'absolute', top:0 })
+		this._taskTitleInput.getElement().focus()
+		// var offsetTop = node.getElement().offsetTop,
+		// 	height = getDocumentHeight(this._doc) - this._header.getOffset().height - this._footer.getOffset().height
+		// node.style({ height:height })
+		// scrollability.scrollTo(this._scrollDiv.getElement(), 0, -offsetTop, 0)
+	}
+	
+	this._onFocusedTaskTitleKeyPress = function() {
+		setTimeout(bind(this, function() {
+			this._focusedTask.title.set(this._taskTitleInput.getElement().value)
+		}))
+	}
+	
+	this._onFocusedTaskTitleBlur = function() {
+		this._taskFocus.remove()
 	}
 })
