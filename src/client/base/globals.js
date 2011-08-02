a58716f8da9c61c3fe304b0a1028adf3c915aac0
Marcus Westin <marcus.westin@gmail.com>
2011-08-01 20:17:07 -0400

    Whitespace fix

--- src/client/base/globals.js
+++ src/client/base/globals.js
@@ -2,9 +2,9 @@ var NODES = require('ui/dom/NODES')
 NODES.exposeGlobals()
 
 NODES.attributeHandlers.data = function(dataProperty) {
-  dataProperty.observe(bind(this, function(dataValue) {
-    this._el.innerHTML = dataValue
-  }))
+	dataProperty.observe(bind(this, function(dataValue) {
+		this._el.innerHTML = dataValue
+	}))
 }
 
 Class = require('std/Class')
