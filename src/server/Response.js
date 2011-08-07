a6344fc54ef3aa5f19b032fe74b91c6eee8ebb4a
Marcus Westin <marcus.westin@gmail.com>
2011-08-07 14:46:05 -0700

    It's process.stderr, not sys.err

--- src/server/Response.js
+++ src/server/Response.js
@@ -34,7 +34,7 @@ module.exports = Class(function() {
 		this._res.writeHead(code || 500)
 		var message = this._printErrors && (err.stack ? err.stack : err.message || err)
 		this._res.end(message)
-		sys.err.write("RESPONSE ERROR code:" + code + " error:" + (err.stack ? err.stack : (err + ": " + new Error().stack)))
+		process.stderr.write("RESPONSE ERROR code:" + code + " error:" + (err.stack ? err.stack : (err + ": " + new Error().stack)) + "\n")
 		return this
 	}
 })
